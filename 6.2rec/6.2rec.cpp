f10f31b22eee9388e64a0f3b36978677f88515f3
Julia <mihajloviculia65@gmail.com>
2022-12-14 22:42:43 +0200

    first master commit

---
+++ 6.2rec/6.2rec.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <cmath>
+#include <time.h>
+
+using namespace std;
+
+void Create(int mas[], const int n, const int Low, const int High, int i)
+{
+	mas[i] = Low + rand() % (High - Low + 1);
+	if (i < n - 1)
+		Create(mas, n, Low, High, i + 1);
+}
+
+void Print(int mas[], const int n, int i)
+{
+	cout << mas[i] << " ";
+	if (i < n - 1)
+		Print(mas, n, i + 1);
+}
+
+
+int Max(int mas[], const int n, int i, int max)
+{
+	if (mas[i] > max)
+		max = mas[i];
+	if (i < n - 1)
+		return Max(mas, n, i + 1, max);
+	else
+		return max;
+}
+
+int Min(int mas[], const int n, int i, int min)
+{
+	if (mas[i] < min)
+		min = mas[i];
+	if (i < n - 1)
+		return Min(mas, n, i + 1, min);
+	else
+		return min;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	const int k = 10;
+	int a[k];
+
+	int Low = -5;
+	int High = 5;
+
+	Create(a, k, Low, High, 0);
+
+	cout << "a[ ";
+	Print(a, k, 0);
+	cout << "]" << endl;
+
+	cout << "Min = " << Min(a, k, 1, a[0]) << endl;
+	cout << "Max = " << Max(a, k, 1, a[0]) << endl;
+	cout << "arithmetic mean = " << (Max(a, k, 1, a[0])
+		+ Min(a, k, 1, a[0])) / 2. << endl;
+
+	return 0;
+}
---
+++ UnitTest6.2rec/UnitTest6.2rec.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../6.2rec/6.2rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62rec
+{
+	TEST_CLASS(UnitTest62rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			const int k = 2;
+			int a[k] = { 1, 2 };
+			t = Max(a, k, 1, a[0]);
+			Assert::AreEqual(2, t);
+		}
+	};
+}
